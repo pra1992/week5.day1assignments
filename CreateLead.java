ef8525b55f89652f733624bc1c4c9f5e393230e2
pra1992 <58290550+pra1992@users.noreply.github.com>
2022-10-02 14:26:50 +0530

    Add files via upload

---
+++ CreateLead.java
@@ -0,0 +1,29 @@
+package testcase;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLead extends BaseClass {
+
+	@Test
+	public  void runCreatelead () {
+
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.name("submitButton")).click();
+		
+}}
+
+
+
+
+
+
